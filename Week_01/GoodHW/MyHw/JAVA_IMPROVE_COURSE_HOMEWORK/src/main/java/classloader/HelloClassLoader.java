9cb7e57a59067a3bd1ad0b9faa43850bd226b23b
zw3237 <850230760@qq.com>
2021-02-08 17:07:05 +0800

    资料

---
+++ Week_01/GoodHW/MyHw/JAVA_IMPROVE_COURSE_HOMEWORK/src/main/java/classloader/HelloClassLoader.java
@@ -0,0 +1,22 @@
+package classloader;
+
+import java.util.Base64;
+
+public class HelloClassLoader extends ClassLoader {
+
+
+    @Override
+    protected Class<?> findClass(String name) throws ClassNotFoundException {
+        String helloBase64 = "yv66vgAAADQAHwoABgARCQASABMIABQKABUAFgcAFwcAGAEABjxpbml0PgEAAygpVgEABENvZGUBAA9MaW5lTnVtYmVyVGFibGUBABJMb2N" + "hbFZhcmlhYmxlVGFibGUBAAR0aGlzAQALTGp2bS9IZWxsbzsBAAg8Y2xpbml0PgEAClNvdXJjZUZpbGUBAApIZWxsby5qYXZhDAAHAAgHABkMABoAGwEAGEhlb" + "GxvIENsYXNzIEluaXRpYWxpemVkIQcAHAwAHQAeAQAJanZtL0hlbGxvAQAQamF2YS9sYW5nL09iamVjdAEAEGphdmEvbGFuZy9TeXN0ZW0BAANvdXQBABVMamF2" + "YS9pby9QcmludFN0cmVhbTsBABNqYXZhL2lvL1ByaW50U3RyZWFtAQAHcHJpbnRsbgEAFShMamF2YS9sYW5nL1N0cmluZzspVgAhAAUABgAAAAAAAgABAAcACA" + "ABAAkAAAAvAAEAAQAAAAUqtwABsQAAAAIACgAAAAYAAQAAAAMACwAAAAwAAQAAAAUADAANAAAACAAOAAgAAQAJAAAAJQACAAAAAAAJsgACEgO2AASxAAAAAQAK" + "AAAACgACAAAABgAIAAcAAQAPAAAAAgAQ";
+        byte[] decode = decode(helloBase64);
+        defineClass(name, decode, 0, decode, decode.length);
+        return super.findClass(name);
+    }
+
+    private void defineClass(String name, byte[] decode, int off, byte[] decode1, int length) {
+    }
+
+    public byte[] decode(String base64) {
+        return Base64.getDecoder().decode(base64);
+    }
+}
